12830351559e0b13f127985dce7e9a9149c2d13a
Theo S <iosdeveloper@tgs.me.uk>
2026-01-02 17:09:43 +0000

    Point depedency to github repo

--- Package.swift
+++ Package.swift
@@ -20,7 +20,20 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(path: "../BasicMath") // relative path to BasicMath directory
+        .package(
+            //path: "../BasicMath" // relative path to BasicMath directory
+            
+            // 1️⃣ The HTTPS URL of the repo that hosts BasicMath
+            url: "https://github.com/theogscott/BasicMath.git",
+            
+            // 2️⃣ The version rule – choose whichever works for you
+            //   .exact("1.0.0")         // exactly this tag/commit
+            //   .upToNextMajor(from: "1.0.0")
+            //   .upToNextMinor(from: "1.2.0")
+               //.branch("main")           // for a rolling dev branch
+            //   .revision("a1b2c3d")      // a specific commit SHA
+            from: "1.0.0"
+            )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
